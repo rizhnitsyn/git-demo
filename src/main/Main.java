9c53c59b125dc4703066ec0327b7770de986ee7c
rizhnitsyn <32108937+rizhnitsyn@users.noreply.github.com>
2018-01-16 19:50:14 +0300

    Update Main.java
    test merge

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Sergei");
+        System.out.println("Hello Sergei, testing!");
+        
     }
 }
